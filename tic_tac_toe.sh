9bfe9b446dffcd072ce0c22022fc90fce6c270f5
Akshaya <akshayachandorkar29@gmail.com>
2020-02-21 18:06:03 +0530

    Added: declared board

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo -----WELCOME to the world of TIC TAC TOE-----
+
+#constant
+TOTAL_CELL=9
+
+
+declare -a board
+
+function resetting_board()
+{
+	board=(. . . . . . . . . .)
+}
